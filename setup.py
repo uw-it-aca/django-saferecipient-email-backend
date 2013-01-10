56c230ac9fdb7ac6a351fd0d5bb9a4cd66ecbd55
Patrick Michaud <pmichaud@uw.edu>
2013-01-10 10:33:34 -0800

    Adding a setup.py, so this can be installed via requirements.txt

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='SafeEmailBackend',
+      version='1.0',
+      description='A django email backend that sends all emails to a given address',
+     )
