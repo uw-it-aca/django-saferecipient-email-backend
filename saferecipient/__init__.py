9d9f10978fc1687f6c8ea27757eab16d023dc384
Jim Laney <jlaney@uw.edu>
2017-04-18 15:07:28 -0700

    adds a couple of tests

---
+++ saferecipient/__init__.py
@@ -0,0 +1,35 @@
+"""SMTP email backend class that only sends email to a safe email address"""
+
+from django.core.mail.backends.smtp import EmailBackend as SMTPEmailBackend
+from email.mime.text import MIMEText
+from django.conf import settings
+
+
+class EmailBackend(SMTPEmailBackend):
+    """
+    Re-routes email, so it goes to, and comes from
+    settings.SAFE_EMAIL_RECIPIENT.
+
+    The original to and from are added to a text file
+    that is attached to the message.
+    """
+    def send_messages(self, email_messages):
+        for message in email_messages:
+            self._safeguard(message)
+        return super(EmailBackend, self).send_messages(email_messages)
+
+    def _safeguard(self, message):
+        originals = (
+            "Original From: %s\nOriginal To: %s\nOriginal CC: %s\n"
+            "Original BCC: %s\n") % (
+                message.from_email, message.to, message.cc, message.bcc)
+
+        message.from_email = settings.SAFE_EMAIL_RECIPIENT
+        message.to = [settings.SAFE_EMAIL_RECIPIENT]
+        message.cc = []
+        message.bcc = []
+        text_attachment = MIMEText(originals)
+        text_attachment.add_header(
+            'Content-disposition',
+            'attachment; filename="original_emails.txt"')
+        message.attach(text_attachment)
--- setup.py
+++ setup.py
@@ -1,8 +1,39 @@
-#!/usr/bin/env python
+import os
+from setuptools import setup
 
-from distutils.core import setup
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/django-saferecipient-email-backend>`_.
+"""
 
-setup(name='SafeEmailBackend',
-      version='1.0',
-      description='A django email backend that sends all emails to a given address',
-     )
+# The VERSION file is created by travis-ci, based on the tag name
+version_path = 'saferecipient/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='SafeEmailBackend',
+    version=VERSION,
+    packages=['saferecipient'],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[
+        'Django',
+    ],
+    license='Apache License, Version 2.0',
+    description='A django email backend that sends all emails to a given address',
+    long_description=README,
+    url='https://github.com/uw-it-aca/django-saferecipient-email-backend',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
