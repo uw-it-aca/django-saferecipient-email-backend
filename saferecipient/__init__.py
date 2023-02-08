81acb0c3a8e245ecc337ab3a7ce0438fab48136c
Jim Laney <jlaney@uw.edu>
2023-02-07 20:10:24 -0800

    add django4 to test matrix

--- saferecipient/__init__.py
+++ saferecipient/__init__.py
@@ -1,5 +1,6 @@
-# Copyright 2022 UW-IT, University of Washington
+# Copyright 2023 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
+
 
 """SMTP email backend class that only sends email to a safe email address"""
 import re
